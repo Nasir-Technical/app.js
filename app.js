80a2cfbcfc0b2750f8a27af37825361b8ec2b1e0
Nasir Ansari <115993017+Nasir-Technical@users.noreply.github.com>
2022-10-31 16:40:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+alert("welcome to my first web");
+alert("My name is nasir");
+
+var b =10;
+alert(b);
+
+var a =10
+var b =20
+var c=a+b;
+alert(c)
+
+var c=b-a;
+var c=b-a;
+var c=b+a;
+var c=b/a;
+alert(c)
